9f4748877aef11833848baee92ff0894dbdb76a1
itakurakoichi <itakurakoichi.w@gmail.com>
2015-12-07 22:54:37 +0900

    fix js syntax

--- src/inline--anim-when-ad-is-appeared/js/base.js
+++ src/inline--anim-when-ad-is-appeared/js/base.js
@@ -8,40 +8,27 @@ $(function() {
 	var adApperedDisdance = $ad.offset().top - $(window).height() + $ad.height();
 	var adPassedDisdance = $ad.offset().top + $ad.height();
 
-	console.log('[DEBUG] adApperedDisdance: ', adApperedDisdance)
-
 	/**
-	 * ページ読み込み時からADがwindow内に表示されていた場合
-	 * 読み込み時にanimationさせるか...
+	 * ページ読み込み時にADがwindow内に存在 -> animationさせる
 	 */
 	if (adApperedDisdance < 0) {
-		console.log('[DEBUG] ADが最初から表示されています')
 		$ad.addClass('anim');
 	}
 
 	/**
-	 * スクロール中に、ADが現れたタイミングで何かしら処理をする
+	 * スクロール中, ADがwindows内に表示されたタイミングでanimation
 	 */
 	var scrollTop;
 	$(window).scroll(function() {
 		scrollTop = $(window).scrollTop();
-		console.log('[DEBUG] scrollTop: ', scrollTop)
 
 		if (scrollTop >= adPassedDisdance) {
-			console.log('[DEBUG] ADが通り過ぎた')
 			$ad.removeClass('anim');
-
 		} else if (scrollTop >= adApperedDisdance) {
-			// ADがwindow内に現れた
-			console.log('[DEBUG] ADがwindow内に現れた')
 			$ad.addClass('anim');
-
 		} else {
-			console.log('[DEBUG] ADがwindow内ではない')
 			$ad.removeClass('anim');
 		}
 	});
-
-	
 
 });
